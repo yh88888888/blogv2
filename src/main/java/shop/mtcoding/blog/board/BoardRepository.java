7922fa1d493b654b126585f16cb8a7ba914f08f8
getinthere <getinthere@naver.com>
2024-03-14 12:24:50 +0900

    게시글 목록보기 - default batch fetch size

--- src/main/java/shop/mtcoding/blog/board/BoardRepository.java
+++ src/main/java/shop/mtcoding/blog/board/BoardRepository.java
@@ -5,18 +5,25 @@ import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Repository
 public class BoardRepository {
     private final EntityManager em;
 
-    public Board findByIdJoinUser(int id){
+    public List<Board> findAll() {
+        Query query = em.createQuery("select b from Board b order by b.id desc", Board.class);
+        return query.getResultList();
+    }
+
+    public Board findByIdJoinUser(int id) {
         Query query = em.createQuery("select b from Board b join fetch b.user u where b.id = :id", Board.class);
         query.setParameter("id", id);
         return (Board) query.getSingleResult();
     }
 
-    public Board findById(int id){
+    public Board findById(int id) {
         // id, title, content, user_id(이질감), created_at
         Board board = em.find(Board.class, id);
         return board;
--- src/test/java/shop/mtcoding/blog/board/BoardRepositoryTest.java
+++ src/test/java/shop/mtcoding/blog/board/BoardRepositoryTest.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
+import java.util.List;
+
 @Import(BoardRepository.class)
 @DataJpaTest
 public class BoardRepositoryTest {
@@ -13,7 +15,20 @@ public class BoardRepositoryTest {
     private BoardRepository boardRepository;
 
     @Test
-    public void findByIdJoinUser_test(){
+    public void findAll_lazyloading_test() { // default_batch_fetch_size : 10
+        List<Board> boardList = boardRepository.findAll();
+        boardList.forEach(board -> {
+            System.out.println(board.getUser().getUsername()); // lazy loading
+        });
+    }
+
+    @Test
+    public void findAll_test() {
+        List<Board> boardList = boardRepository.findAll();
+    }
+
+    @Test
+    public void findByIdJoinUser_test() {
         int id = 1;
 
         boardRepository.findByIdJoinUser(id);
@@ -22,11 +37,8 @@ public class BoardRepositoryTest {
     @Test
     public void findById_test() {
         int id = 1;
-        System.out.println("start - 1");
         Board board = boardRepository.findById(id);
-        System.out.println("start - 2");
         System.out.println(board.getUser().getId());
-        System.out.println("start - 3");
         System.out.println(board.getUser().getUsername());
     }
 }
